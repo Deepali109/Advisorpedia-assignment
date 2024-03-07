9f9bf6734341fed18243e57f0113516b5b6c2695
Deepali <deepalisahu965@gmail.com>
2024-03-07 16:19:10 +0530

    name changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../components/Header';
-// import { showsampledata } from '../redux/action/action'
+
 import PostCards from './Postcards';
 
 function Home() {
-  // const {sample} = useSelector(state => state.getSampledata);
-  // console.log(sample)
-  
-  // const dispatch = useDispatch();
-  
-  // useEffect(()=>{
-  //     dispatch(showsampledata());
-  // },[dispatch])
-
-
   return (
     <div>
-    <Header/>
-      {/* <PostCards  sample={sample} /> */}
+    {/* <Header/> */}
       <PostCards/>
     </div>
   )
